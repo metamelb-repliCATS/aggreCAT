94971b5c0534c674c264cfb3ac2b605a52249cc0
Aaron Willcox <awillcox@deakin.edu.au>
2023-03-27 13:09:16 +1100

    - Ported original ridgeplot PR #30 issue #33

--- R/confidence_score_plots.R
+++ R/confidence_score_plots.R
@@ -1,23 +1,18 @@
 #' Confidence Score Ridge Plot
 #'
-#' Display a ridge plot of confidence scores for each aggregation method.
+#' Display a ridge plot of confidence scores for each aggregation method faceted by its linear, non-linear
+#' and Bayesian characteristic.
 #'
 #' @param confidence_scores A data frame of confidence scores in long format in the form of [data_confidence_scores]
-#' @param size Font size
-#' @param font Set font family. Defaults to serif
 #'
 #' @return A density ridge plot of aggregation methods
 #'
 #' @examples
-#' \dontrun{confidence_scores_ridgeplot(data_confidence_scores, size = 12,
-#' font = "sans",
-#' soft_colour = "grey30")}
+#' \dontrun{confidence_scores_ridgeplot(data_confidence_scores)}
 #'
 #'
 #' @export
-confidence_score_ridgeplot <- function(confidence_scores = NULL,
-                                       size = 12,
-                                       font = "serif"){
+confidence_score_ridgeplot <- function(confidence_scores = NULL){
 
   if(is.null(confidence_scores)){
     cli::cli_alert_info("No Confidence Scores Provided. Using Package Default")
@@ -34,18 +29,16 @@ confidence_score_ridgeplot <- function(confidence_scores = NULL,
     ggplot2::ggplot(ggplot2::aes(x = cs,
                                  y = factor(method,
                                             levels = rev(levels(factor(method)))),
-                                 fill = after_stat(x),
-                                 colour = cs
+                                 fill = factor(stat(quantile))
     )) +
-    ggridges::geom_density_ridges_gradient(scale = 4,
-                                           col = "white",
-                                           quantile_lines = TRUE, # median line
-                                           quantiles = 2,
-                                           alpha = 0.7,
-                                           #show.legend = FALSE,
-                                           panel_scaling = TRUE) +
-    ggplot2::scale_x_continuous(expand = c(0,0),
-                                breaks = c(0, .25, .5, .75, 1)) +
+    ggridges::stat_density_ridges(
+      geom = "density_ridges_gradient",
+      calc_ecdf = TRUE,
+      quantiles = c(0.025, 0.975)) +
+    ggplot2::scale_fill_manual(
+      name = "Probability", values = c("#D55E00", "#A0A0A0A0", "#0072B2"), # colour friendly
+      labels = c("(0, 0.025]", "(0.025, 0.975]", "(0.975, 1]")) +
+    ggplot2::scale_x_continuous(breaks = c(0, .25, .5, .75, 1)) +
     ggplot2::scale_y_discrete(expand = c(0,0)) +
     ggplot2::geom_vline(xintercept=.50,
                         linetype="dashed",
@@ -55,57 +48,25 @@ confidence_score_ridgeplot <- function(confidence_scores = NULL,
     ggplot2::theme(axis.ticks = ggplot2::element_blank(),
                    axis.ticks.x = ggplot2::element_blank(),
                    axis.ticks.y = ggplot2::element_blank(),
+                   axis.text.y = ggplot2::element_text(face = "bold"),
+                   axis.text.x = ggplot2::element_text(face = "bold"),
                    axis.line = ggplot2::element_blank(),
                    panel.grid = ggplot2::element_blank(),
                    panel.background = ggplot2::element_rect(fill = "transparent",
                                                             colour = NA),
                    plot.background = ggplot2::element_rect(fill = "white",
                                                            colour = NA),
-                   strip.text = ggplot2::element_text(face = "italic",
-                                                      family = font),
                    strip.placement = "outside",
                    strip.background = ggplot2::element_blank(),
-                   axis.text.x = ggplot2::element_text(family = font,
-                                                       size = size * 0.8,
-                                                       margin = ggplot2::margin(t = 0)),
-                   axis.text.y = ggplot2::element_text(family = font,
-                                                       size = size * 0.8,
-                                                       margin = ggplot2::margin(r = 0)),
-                   axis.title = ggplot2::element_text(size = size * 0.85,
-                                                      family = font),
-                   axis.title.x = ggplot2::element_text(size = size * 0.85,
-                                                        family = font,
-                                                        face = "bold"),
-                   plot.title = ggplot2::element_text(size = size * 1.25,
-                                                      margin = ggplot2::margin(b = size * 0.3),
-                                                      family = font,
-                                                      face = "bold",
-                                                      color = "black"),
-                   plot.title.position = "plot",
-                   plot.subtitle = ggplot2::element_text(hjust = 0,
-                                                         size = size * 1.0,
-                                                         margin = ggplot2::margin(b = size * 0.5),
-                                                         family = font,
-                                                         face = "italic"),
-                   plot.caption = ggplot2::element_text(hjust = 1,
-                                                        size = size * 0.85,
-                                                        margin = ggplot2::margin(t = size * 0.9),
-                                                        family = font),
-                   plot.margin = ggplot2::margin(9, 9, 9, 9),
-    ) +
-    ggplot2::guides(fill = "none",
-                    linetype = "none") +
-    ggplot2::labs(title = NULL,
+                   strip.text = ggplot2::element_text(face = "bold"),
+                   plot.title = ggplot2::element_text(face = "bold"),
+                   plot.title.position = "plot") +
+    ggplot2::labs(title = "Density Plot of Aggregated Confidence Scores",
                   subtitle = paste0("Claims Assessed N = ", number_of_claims),
                   x = "Predicted Replication Scores",
                   y = "Aggregation Method",
-                  caption = "Prediction cutoff threshold depicted by dashed line (0.5). Median depicted by solid line") +
-    ggplot2::facet_wrap(~ type,
-                        scales =  "free",
-                        shrink = FALSE,
-                        dir = "v",
-                        strip.position = "top")
-
+                  caption = "Prediction cutoff threshold depicted by dashed line (0.5)") +
+    ggforce::facet_row(~type, scales = "free", space = "fixed")
 }
 
 
@@ -114,14 +75,14 @@ confidence_score_ridgeplot <- function(confidence_scores = NULL,
 #' Confidence scores displayed on a colour spectrum across generated methods and assessed claims,
 #' split into predicted replication outcomes (TRUE/FALSE). White indicative of around `.5` with higher predicted
 #' confidence scores more blue (`>.5`) and lower more red (`<.5`). Each predicted replication outcome is then
-#' split into the group type of the underlying statistical characteristic for each aggregation method.
+#' split into the group type of the underlying statistical characteristic for each aggregation method (non-weighted linear,
+#' weighted linear & Bayesian).
 #'
 #' @param confidence_scores A data frame of confidence scores generated from the aggregation methods in the form of [data_confidence_scores].
 #' Defaults to [data_confidence_scores] if no argument supplied.
 #' @param data_outcomes A data frame of unique claims and the associated binary outcome in the form of [data_outcomes]. If no argument supplied
 #' then defaults to [data_outcomes] supplied within package.
-#' @param x_label Bottom x axis label name. Default is blank.
-#' @param font Font family for plots aesthetics. Defaults to serif
+#' @param x_label Bottom x axis label name or ID. Default is blank.
 #'
 #' @importFrom insight format_capitalize
 #'
@@ -134,8 +95,7 @@ confidence_score_ridgeplot <- function(confidence_scores = NULL,
 
 confidence_score_heatmap <- function(confidence_scores = NULL,
                                      data_outcomes = NULL,
-                                     x_label = NULL,
-                                     font = "serif"){
+                                     x_label = NULL){
 
 
   # Defaults to example dataset if none provided
@@ -197,9 +157,9 @@ confidence_score_heatmap <- function(confidence_scores = NULL,
                  y = factor(method, levels = rev(levels(factor(method)))),
                  fill = cs,
                  colour = cs) +
-    ggplot2::geom_tile(size = 1, linejoin = "mitre") +
-    ggplot2::scale_fill_distiller(palette = "RdBu", direction = 1) +
-    ggplot2::scale_color_distiller(palette = "RdBu", direction = 1) +
+    ggplot2::geom_tile(linetype = 1, linejoin = "mitre") +
+    ggplot2::scale_fill_distiller(palette = "RdYlBu", direction = 1) +
+    ggplot2::scale_color_distiller(palette = "RdYlBu", direction = 1) +
     ggplot2::labs(x = "",
                   y = "Aggregation Method",
                   title = "",
@@ -207,19 +167,19 @@ confidence_score_heatmap <- function(confidence_scores = NULL,
                   caption = "",
                   fill = "Confidence \nScore", color = "Confidence \nScore") +
     ggplot2::theme(
-      axis.text.y = ggplot2::element_text(face = "bold", family = font),
-      axis.text.x = ggplot2::element_text(angle = 75, hjust = 1, family = font),
-      plot.title = ggplot2::element_text(face = "bold", family = font),
-      plot.subtitle = ggplot2::element_text(family = font),
+      axis.text.y = ggplot2::element_text(face = "bold"),
+      axis.text.x = ggplot2::element_text(angle = 75, hjust = 1),
+      plot.title = ggplot2::element_text(face = "bold"),
+      plot.subtitle = ggplot2::element_text(),
       axis.ticks.y = ggplot2::element_blank(),
       legend.position = "none",
-      legend.text = ggplot2::element_text(family = font),
-      legend.title =  ggplot2::element_text(family = font),
+      legend.text = ggplot2::element_text(),
+      legend.title =  ggplot2::element_text(),
       panel.border = ggplot2::element_blank(),
       strip.background = ggplot2::element_blank(),
       plot.background = ggplot2::element_blank(),
       panel.background = ggplot2::element_blank(),
-      strip.text = ggplot2::element_text(face = "italic", family = font)
+      strip.text = ggplot2::element_text(face = "bold")
     ) +
     ggforce::facet_col(~type, scales = "free_y", space = "free")
 
@@ -234,9 +194,9 @@ confidence_score_heatmap <- function(confidence_scores = NULL,
     ggplot2::aes(x = paper_id, y = factor(method, levels = rev(levels(factor(method)))),
                  fill = cs,
                  colour = cs) +
-    ggplot2::geom_tile(size = 1, linejoin = "mitre") +
-    ggplot2::scale_fill_distiller(palette = "RdBu", direction = 1) +
-    ggplot2::scale_color_distiller(palette = "RdBu", direction = 1) +
+    ggplot2::geom_tile(linetype = 1, linejoin = "mitre") +
+    ggplot2::scale_fill_distiller(palette = "RdYlBu", direction = 1) +
+    ggplot2::scale_color_distiller(palette = "RdYlBu", direction = 1) +
     ggplot2::labs(x = "",
                   y = "Aggregation Method",
                   title = "",
@@ -244,26 +204,26 @@ confidence_score_heatmap <- function(confidence_scores = NULL,
                   caption = "",
                   fill = "Confidence \nScore", color = "Confidence \nScore") +
     ggplot2::theme(
-      axis.text.x = ggplot2::element_text(angle = 75, hjust = 1, family = font),
+      axis.text.x = ggplot2::element_text(angle = 75, hjust = 1),
       axis.text.y = ggplot2::element_blank(),
       axis.ticks.y = ggplot2::element_blank(),
       axis.title.y = ggplot2::element_blank(),
-      plot.title = ggplot2::element_text(face = "bold", family = font),
-      plot.subtitle = ggplot2::element_text(family = font),
+      plot.title = ggplot2::element_text(face = "bold"),
+      plot.subtitle = ggplot2::element_text(),
       legend.position = "none",
-      legend.text = ggplot2::element_text(family = font),
-      legend.title =  ggplot2::element_text(family = font),
+      legend.text = ggplot2::element_text(),
+      legend.title =  ggplot2::element_text(),
       panel.border = ggplot2::element_blank(),
       strip.background = ggplot2::element_blank(),
       plot.background = ggplot2::element_blank(),
       panel.background = ggplot2::element_blank(),
-      strip.text = ggplot2::element_text(face = "italic", family = font)
+      strip.text = ggplot2::element_text(face = "bold")
     ) +
     ggforce::facet_col(~type, scales = "free_y", space = "free")
 
   figure <- ggpubr::ggarrange(plot1, plot2,
                               #labels = c("A", "B"),
-                              font.label = font,
+                              #font.label = font,
                               common.legend = TRUE,
                               legend = "right",
                               heights = c(2,1),
@@ -273,7 +233,7 @@ confidence_score_heatmap <- function(confidence_scores = NULL,
 
   ggpubr::annotate_figure(figure,
                           bottom = ggpubr::text_grob(x_label,
-                                                     family = font,
+
                                                      just = "centre"))
 }
 
@@ -285,8 +245,7 @@ method_types <- function(conf_scores){
   WLCI = c("ShiftWAgg", "BestShiftWAgg", "IntShiftWAgg", "DistShiftWAgg", "DistIntShiftWAgg",
            "IntWAgg", "IndIntWAgg", "AsymWAgg", "IndIntAsymWAgg", "VarIndIntWAgg", "KitchSinkWAgg",
            "DistLimitWAgg", "GranWAgg", "OutWAgg", "BetaArMean", "BetaArMean2", "DistribArMean", "TriDistribArMean",
-           "CompWAgg", "EngWAgg")
-  WLCE = c("ReasonWAgg", "ReasonWAgg2", "QuizWAgg", "QuizWAgg2", "QuizWAgg3",
+           "CompWAgg", "EngWAgg","ReasonWAgg", "ReasonWAgg2", "QuizWAgg", "QuizWAgg2", "QuizWAgg3",
            "BetaReasonWAgg", "BetaReasonWAgg2", "BadgeWAgg",
            "ExperienceWAgg", "ExperienceWAgg2", "ExperienceWAgg3")
   BAYES = c("BayTriVar", "BayPRIORsAgg")
@@ -294,7 +253,7 @@ method_types <- function(conf_scores){
   conf_scores <- conf_scores %>%
     dplyr::mutate(type = dplyr::case_when(method %in% NWL ~ "Non-weighted Linear Combintation",
                                           method %in% WLCI ~ "Weighted Linear Combinations",
-                                          method %in% WLCE ~ "Weighted Linear Combinations (Supplementary Data)",
+                                          #method %in% WLCE ~ "Weighted Linear Combinations (Supplementary Data)",
                                           method %in% BAYES ~ "Bayesian Methods"))
 
 
@@ -302,7 +261,7 @@ method_types <- function(conf_scores){
   conf_scores$type <- factor(conf_scores$type,
                              levels = c("Non-weighted Linear Combintation",
                                         "Weighted Linear Combinations",
-                                        "Weighted Linear Combinations (Supplementary Data)",
+                                        #"Weighted Linear Combinations (Supplementary Data)",
                                         "Bayesian Methods"))
 
   return(conf_scores)
