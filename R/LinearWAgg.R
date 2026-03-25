d480e24345673ee7b6806e2aea0f0d67c6863c34
Doi90 <david.wilkinson.research@gmail.com>
2026-03-25 15:44:41 +1100

    Same fix for LinearWAgg

--- R/LinearWAgg.R
+++ R/LinearWAgg.R
@@ -61,7 +61,7 @@
 #' @return A tibble of confidence scores `cs` for each `paper_id`.
 #'
 #' @examples
-#' \donttest{LinearWAgg(data_ratings)}
+#' \donttest{LinearWAgg(data_ratings, percent_toggle = TRUE)}
 #'
 #' @export
 #' @md
