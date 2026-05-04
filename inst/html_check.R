3a648d9ee3f7106c412e281fd656568f816bbb57
egouldo <egouldo@users.noreply.github.com>
2026-05-04 22:17:40 +1000

    - Fix  html check
    move stopifnot to correct location -> *after* validation. And call Version dynamically.

--- inst/html_check.R
+++ inst/html_check.R
@@ -5,12 +5,10 @@
 # MUST: devtools::document()
 # MUST: devtools::build()
 
-src <- here::here("../aggreCAT_1.0.1.tar.gz")   # or just PKG if installed
-out <- here::here("aggreCAT.html")
+PKG_name_version <- glue::glue('{packageDescription("aggreCAT")$Package}_{packageDescription("aggreCAT")$Version}')
 
-stopifnot(
-    all(fs::file_exists(c(src, out)))
-)
+src <- glue::glue("../{PKG_name_version}.tar.gz")   # or just PKG if installed
+out <- "aggreCAT.html"
 
 tools::pkg2HTML(src, out)
 library(W3CMarkupValidator)
@@ -18,5 +16,9 @@ library(W3CMarkupValidator)
 results <- w3c_markup_validate(file = out, jar = TRUE)
 
 if (length(results$type) > 0) results |> tibble::as_tibble()
+
+stopifnot(
+    all(fs::file_exists(c(src, out)))
+)
 
 file.remove(out)
